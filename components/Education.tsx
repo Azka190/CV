304d0fea3e41ddf832dd414853e20512d2dad592
Azka190 <azkarehman2001@gmail.com>
2024-07-14 19:06:06 +0500

    1

--- components/Education.tsx
+++ components/Education.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 function Education() {
   return (
     <div className="p-4">
-      <div className='text-white font-mono text-4xl md:text-6xl bg-pink-200 shadow-lg hover:shadow-pink-500 flex justify-center mt-7 mx-auto max-w-4xl p-4'>
+      <div className="text-white font-mono text-4xl md:text-6xl bg-pink-200 shadow-lg hover:shadow-pink-500 flex justify-center mt-7 mx-auto max-w-4xl p-4">
         <h1>My Education</h1>
       </div>
-      <div className='flex md:flex-row flex-wrap justify-center  p-4 md:p-10'>
+      <div className="flex flex-col md:flex-row flex-wrap justify-center p-4 md:p-10">
         {/* <div className='p-4 text-center border border-spacing-5 border-blue-500 shadow-lg shadow-blue-500 rounded-full mx-4 md:mx-20 my-4 md:my-20'>
           <h1 className='text-xl md:text-2xl font-bold underline underline-offset-1'>Matric</h1>
           <p>I completed my 10th from Govt Malikwal and got 800 marks.</p>
         </div> */}
-        <div className='p-4 text-center border border-spacing-5 border-blue-500 shadow-lg shadow-blue-500 rounded-full mx-4 md:mx-20 my-4 md:my-20'>
-          <h1 className='text-xl md:text-2xl font-bold underline underline-offset-1'>Software Engineering</h1>
-          <p>
-          I am a student at Virtual University, currently developing a Full Stack Job Portal as my Final Year Project (FYP). This project involves creating a comprehensive platform for job seekers and employers, integrating front-end and back-end technologies to deliver a seamless user experience.          </p>
+        <div className="p-4 text-center border border-spacing-5 border-blue-500 shadow-lg shadow-blue-500 rounded-full mx-4 my-4 md:mx-20 md:my-20 max-w-xs md:max-w-none">
+          <h1 className="text-lg md:text-2xl font-bold underline underline-offset-1">Software Engineering</h1>
+          <p className="text-sm md:text-base">
+            I am a student at Virtual University, currently developing a Full Stack Job Portal as my Final Year Project (FYP). This project involves creating a comprehensive platform for job seekers and employers, integrating front-end and back-end technologies to deliver a seamless user experience.
+          </p>
         </div>
         {/* <div className='p-4 text-center border border-spacing-5 border-blue-500 shadow-lg shadow-blue-500 rounded-full mx-4 md:mx-20 my-4 md:my-20'>
           <h1 className='text-xl md:text-2xl font-bold underline underline-offset-1'>FSC</h1>
@@ -22,7 +23,7 @@ function Education() {
         </div> */}
       </div>
     </div>
-  )
+  );
 }
 
-export default Education
+export default Education;
--- components/Hireme.tsx
+++ components/Hireme.tsx
@@ -1,39 +1,51 @@
-import React from 'react'
+import React from 'react';
 
 function Hireme() {
   return (
-    <div >
-          {/* BAckground images */}
-        <div>
-          <img className="flex relative animate-bounce-slow" src="https://denisechandler.com/wp-content/themes/portfolio_oct2021/images/swish_solid.png" alt="robot" />
+    <div>
+      {/* Background images */}
+      <div>
+        <img
+          className="flex relative animate-bounce-slow"
+          src="https://denisechandler.com/wp-content/themes/portfolio_oct2021/images/swish_solid.png"
+          alt="robot"
+        />
+      </div>
+      {/* I love working with you */}
+      <div className="bg-regal-blue flex flex-col md:flex-row gap-10 p-4 md:p-16">
+        <div className="md:ml-10 md:mr-10 flex flex-col items-center md:items-start">
+          <div className="flex items-center">
+            <p className="text-white text-2xl md:text-4xl font-mono pr-2">I</p>
+            <img
+              className="relative animate-pulse w-8 md:w-12 p-1"
+              src="https://denisechandler.com/wp-content/themes/portfolio_oct2021/images/heart2.png"
+              alt="heart"
+            />
+            <p className="text-white text-2xl md:text-4xl font-mono pl-2">Working With You</p>
+          </div>
+          {/* Main text */}
+          <div className="pt-4 md:pt-9 pb-2 md:pb-5">
+            <p className="font-mono text-sm md:text-base">
+              Although I am new to the field, I have a strong passion for both design and coding. I’ve always been someone who has both a creative and a logical side. When I discovered front-end development, I realized it was the perfect fit. I could use my creative side to design user interfaces and my logical side to write clean and efficient code. While I may be new to the field, I am dedicated, hardworking, and always looking for exciting opportunities to grow. Take a look at my portfolio below, and if you think I’d be a good match for an internship, please send me an email.
+            </p>
+            <a
+              href="mailto:azkarehman2001@gmail.com"
+              className="text-xl md:text-2xl text-white font-mono hover:underline hover:decoration-wavy"
+            >
+              Send me Mail
+            </a>
+          </div>
         </div>
-          {/* i love working with you  */}
-          <div className='bg-regal-blue flex gap-10 '>
-            <div className=' p-16 ml-10 mr-10'>
-              <div className='flex'>
-                <p className='text-white text-4xl font-mono pr-2'>
-                  I
-                </p>
-                <img className="flex relative animate-pulse w-12 p-1" src="https://denisechandler.com/wp-content/themes/portfolio_oct2021/images/heart2.png" alt="robot" />
-                <p className='text-white text-4xl font-mono pl-2'> 
-                  Working With You
-                </p>
-              </div>
-                {/* Main text */}
-              <div >
-                <p className='font-mono pt-9 pb-5'>
-                Although I am new to the field, I have a strong passion for both design and coding. I’ve always been someone who has both a creative and a logical side. When I discovered front-end development, I realized it was the perfect fit. I could use my creative side to design user interfaces and my logical side to write clean and efficient code.While I may be new to the field, I am dedicated, hardworking, and always looking for exciting opportunities to grow. Take a look at my portfolio below, and if you think I’d be a good match for an internship, please send me an email.
-                </p>
-                <a href="mailto:azkarehman2001@gmail.com" className="text-2xl text-white font-mono hover:underline hover:decoration-wavy ">Send me Mail</a>
-              </div>
-            </div>
-            <div className='pr-52'>
-              <img className="flex relative -mt-16  w-[250vw] h-[80vh] " src="girl.png" alt="robot" />
-            </div>
+        <div className="flex justify-center md:pr-52">
+          <img
+            className="relative -mt-10 md:-mt-16 w-[80vw] md:w-[250vw] h-[50vh] md:h-[80vh]"
+            src="girl.png"
+            alt="robot"
+          />
         </div>
-        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hireme
+export default Hireme;
